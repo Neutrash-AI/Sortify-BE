77b244c7c6fce7cb485aee81acf1c518142afbb0
eben4ya <benaya.imanuela@gmail.com>
2025-03-27 21:27:33 +0700

    fix: cors
    allow all host to access resources from BE

--- src/config/socketConfig.ts
+++ src/config/socketConfig.ts
@@ -6,7 +6,7 @@ export default (server: any) => {
   const io = new Server(server, {
     // ! This is a security risk, only use it for development
     cors: {
-      origin: "http://localhost:5173",
+      origin: "*",
     },
     maxHttpBufferSize: 1e8, // max buffer size (100 MB) for http messages
     serveClient: false, // we use separate socket.io client in frontend
