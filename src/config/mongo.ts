5be6d809e4d586ff9aeb09f63da15fe9816f45d9
eben4ya <benaya.imanuela@gmail.com>
2025-05-16 06:41:06 +0700

    feat: mongodb connection

---
+++ src/config/mongo.ts
@@ -0,0 +1,12 @@
+import { MongoClient } from "mongodb";
+
+const uri: string = process.env.MONGODB_URI || "";
+let cachedClient: MongoClient | null = null;
+
+export async function connectToDatabase() {
+  if (cachedClient) return cachedClient;
+  const client = new MongoClient(uri);
+  await client.connect();
+  cachedClient = client;
+  return client;
+}
